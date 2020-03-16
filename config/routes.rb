1b57e95cfe29d95e6b943022885c504a11bf7420
Iskander Dino <iskanderdino@yahoo.com>
2020-03-17 05:18:12 +0800

    add signup page and redirect user from new to signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  
-  resources :users
+
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
